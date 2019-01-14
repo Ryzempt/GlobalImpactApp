a166994fd2291ebed3bbaaf7dd99482008ab3c19
Robbie <robbiekaufman0504@gmail.com>
2019-01-14 09:09:32 -0700

    created app

---
+++ GlobalImpactApp/Controller/ImpactViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  GlobalImpactApp
+//
+//  Created by Kaufman, Robert on 1/10/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ImpactViewController: UIViewController {
+   
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var impactImage: UIImageView!
+    @IBOutlet weak var animateButton: UIButton!
+    
+  
+    
+   
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+
+
+    @IBAction func animateClick(_ sender: Any)
+    {
+    let impactLocation = CLLocationCoordinate2D(latitude: -28.6657, longitude: 30.5296)
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 10.05, longitudeDelta: 10.05)
+    let impactRegion = MKCoordinateRegion(center: impactLocation, span: mapSpan)
+    mapView.animatedZoom(zoomRegion: impactRegion, duration: 2.5)
+    }
+}
+    extension MKMapView
+    {
+        public func animatedZoom( zoomRegion: MKCoordinateRegion, duration: TimeInterval)
+        {
+            MKMapView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 10, options: UIView.AnimationOptions.curveEaseIn, animations:
+                { self.setRegion(zoomRegion, animated: true) }, completion: nil)
+        }
+        
+    }
+
+
+
+
